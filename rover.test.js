8098ce3a3dc56b053dc45215191225f48ac8e9db
Tchakpele <k.tchakpele@gmail.com>
2020-01-27 14:07:22 +0100

    update

--- rover.test.js
+++ rover.test.js
@@ -149,7 +149,11 @@ function deplacer(rover,commande) {
         a : { E: 1, O: -1, N: 0, S: 0 },
 
 
-        r : { E: -1, O: 1, N: 0, S: 0 }
+        r: { E: -1, O: 1, N: 0, S: 0 },
+
+        d: { E: 0, O: 0, N: 0, S: 0 },
+
+        g: { E: 0, O: 0, N: 0, S: 0 },
 
     };
 
@@ -158,21 +162,38 @@ function deplacer(rover,commande) {
         a : { E: 0, O: 0, N: 1, S: -1 },
 
 
-        r : { E: 0, O: 0, N: -1, S: 1 }
+        r: { E: 0, O: 0, N: -1, S: 1 },
+
+        d: { E: 0, O: 0, N: 0, S: 0 },
+
+        g: { E: 0, O: 0, N: 0, S: 0 },
 
     };
 
+    let tab = ['N', 'E', 'S', 'O'];
+
+    let x = tab.indexOf(rover.orientation);
+
+    let sens = { d: 1, g: -1 , a: 0, r: 0};
+
+
+    let new_orientation = ((x + sens[commande]) + 4) % 4;
+
+    let orientationactuelle = tab[new_orientation];
 
     let mapx = actionx[commande];
 
     let mapy = actiony[commande];
 
-    const valeurx = ((rover.position.x + mapx[rover.orientation]) + 50) % 50;
-    const valeury = ((rover.position.y + mapy[rover.orientation]) + 50) % 50;
+    const valeurx = ((rover.position.x + mapx[orientationactuelle]) + 50) % 50;
+    const valeury = ((rover.position.y + mapy[orientationactuelle]) + 50) % 50;
 
-    const newposition = { x: valeurx, y: valeury };
+    const position = { x: valeurx, y: valeury };
 
-    return { ...rover, newposition};
+    const orientation = orientationactuelle;
+
+
+    return { position,orientation };
 
 
 
@@ -190,7 +211,8 @@ test('avancer  en étant à E avec le rover à la  position x:0 , y:0  on doit avoi
 
     const nouvelleposition = deplacer(rover,'a');
 
-    expect(nouvelleposition.newposition).toEqual({x:1 , y:0 });
+    expect(nouvelleposition.position).toEqual({ x: 1, y: 0 });
+    expect(nouvelleposition.orientation).toEqual("E");
 
 
 
@@ -209,8 +231,8 @@ test('avancer  en étant à O avec le rover à la  position x:0 , y:0  on doit avoi
 
     const nouvelleposition = deplacer(rover,'a');
 
-    expect(nouvelleposition.newposition).toEqual({ x: 49, y: 0 });
-
+    expect(nouvelleposition.position).toEqual({ x: 49, y: 0 });
+    expect(nouvelleposition.orientation).toEqual("O");
 
 
 
@@ -228,7 +250,9 @@ test('avancer  en étant à E avec le rover à la  position x:49 , y:0  on doit avo
 
     const nouvelleposition = deplacer(rover,'a');
 
-    expect(nouvelleposition.newposition).toEqual({ x: 0, y: 0 });
+    expect(nouvelleposition.position).toEqual({ x: 0, y: 0 });
+    expect(nouvelleposition.orientation).toEqual("E");
+
 
 
 
@@ -247,7 +271,8 @@ test('avancer  en étant à N avec le rover à la  position x:0 , y:0  on doit avoi
 
     const nouvelleposition = deplacer(rover,'a');
 
-    expect(nouvelleposition.newposition).toEqual({ x: 0, y: 1 });
+    expect(nouvelleposition.position).toEqual({ x: 0, y: 1 });
+    expect(nouvelleposition.orientation).toEqual("N");
 
 
 
@@ -267,7 +292,8 @@ test('avancer  en étant à N avec le rover à la  position x:0 , y:49  on doit avo
 
     const nouvelleposition = deplacer(rover,'a');
 
-    expect(nouvelleposition.newposition).toEqual({ x: 0, y: 0 });
+    expect(nouvelleposition.position).toEqual({ x: 0, y: 0 });
+    expect(nouvelleposition.orientation).toEqual("N");
 
 
 
@@ -286,7 +312,8 @@ test('reculer  en étant à N avec le rover à la  position x:0 , y:0  on doit avoi
 
     const nouvelleposition = deplacer(rover,'r');
 
-    expect(nouvelleposition.newposition).toEqual({ x: 0, y: 49 });
+    expect(nouvelleposition.position).toEqual({ x: 0, y: 49 });
+    expect(nouvelleposition.orientation).toEqual("N");
 
 
 
@@ -305,13 +332,76 @@ test('reculer  en étant à O avec le rover à la  position x:0 , y:0  on doit avoi
 
     const nouvelleposition = deplacer(rover, 'r');
 
-    expect(nouvelleposition.newposition).toEqual({ x: 1, y: 0 });
+    expect(nouvelleposition.position).toEqual({ x: 1, y: 0 });
+    expect(nouvelleposition.orientation).toEqual( "O" );
 
 
 
 
 
 });
+
+function update(rover, liste_action) {
+
+
+    var step_move = liste_action.reduce(function (accumulateur, curent_val, index, array) {
+        console.log(accumulateur, curent_val);
+        return deplacer(accumulateur, curent_val);
+    },rover);
+
+
+    var last_move = step_move;
+
+    return last_move;
+
+}
+
+test('serie daction avance 3 fois en étant au N avec le rover à la  position x:0 , y:0  , on doit avoir comme resultat x:0 y:3 orientation N', () => {
+
+    let Liste_action = ["a", "a", "a"];
+
+
+    let position = { x: 0, y: 0 };
+
+    let orientation = 'N';
+
+    const rover = create_rover_position(position, orientation);
+
+
+    const nouvelleposition = update(rover, Liste_action);
+
+    expect(nouvelleposition.position).toEqual({ x: 0, y: 3 });
+    expect(nouvelleposition.orientation).toEqual("N");
+
+
+
+
+
+});
+
+test('serie daction avance 2 fois ,puis oriente toi vers la droite et recule 1 fois en étant au N avec le rover à la  position x:0 , y:0  , on doit avoir comme resultat x:49 y:2 orientation O', () => {
+
+    let Liste_action = [ "a","a","d","r"];
+
+
+    let position = { x: 0, y: 0 };
+
+    let orientation = 'N';
+
+    const rover = create_rover_position(position, orientation);
+
+
+    const nouvelleposition = update(rover, Liste_action);
+
+    expect(nouvelleposition.position).toEqual({ x: 49, y: 2 });
+    expect(nouvelleposition.orientation).toEqual("E");
+
+
+
+
+
+});
+
 
 
 
